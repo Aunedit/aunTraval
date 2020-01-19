b69898f86435dde865a454b45401c11f14d7d3bd
aunedit <20online02.vivos1pro@gmail.com>
2020-01-19 15:56:53 +0700

    Finish 19

--- lib/widget/authen.dart
+++ lib/widget/authen.dart
@@ -1,5 +1,8 @@
 import 'package:auntravel/utility/my_style.dart';
+import 'package:auntravel/utility/normal_dialog.dart';
 import 'package:auntravel/widget/register.dart';
+import 'package:auntravel/widget/travel.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Authen extends StatefulWidget {
@@ -10,6 +13,8 @@ class Authen extends StatefulWidget {
 class _AuthenState extends State<Authen> {
   //  Feild
 
+  String user = '', password = '';
+
   // Method
 
   Widget singInButton() {
@@ -19,8 +24,25 @@ class _AuthenState extends State<Authen> {
         'Sign In',
         style: TextStyle(color: Colors.white),
       ),
-      onPressed: () {},
+      onPressed: () {
+        checkAuthen();
+      },
     );
+  }
+
+  Future<void> checkAuthen() async {
+    FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+    await firebaseAuth
+        .signInWithEmailAndPassword(email: user, password: password)
+        .then((response) {
+          MaterialPageRoute materialPageRoute = MaterialPageRoute(builder: (BuildContext buildContext)=>Travel());
+          Navigator.of(context).pushAndRemoveUntil(materialPageRoute , (Route<dynamic> route)=>false);
+        })
+        .catchError((response) {
+          String title = response.code;
+          String message = response.message;
+          normalDialog(context, title, message);
+        });
   }
 
   Widget singUpButton() {
@@ -58,6 +80,9 @@ class _AuthenState extends State<Authen> {
     return Container(
       width: 250.0,
       child: TextField(
+        onChanged: (String string) {
+          user = string.trim();
+        },
         keyboardType: TextInputType.emailAddress,
         decoration: InputDecoration(
           enabledBorder: UnderlineInputBorder(
@@ -77,6 +102,9 @@ class _AuthenState extends State<Authen> {
     return Container(
       width: 250.0,
       child: TextField(
+        onChanged: (String string) {
+          password = string.trim();
+        },
         obscureText: true,
         decoration: InputDecoration(
           enabledBorder: UnderlineInputBorder(
--- lib/widget/register.dart
+++ lib/widget/register.dart
@@ -1,6 +1,11 @@
 import 'dart:io';
+import 'dart:math';
 
 import 'package:auntravel/utility/my_style.dart';
+import 'package:auntravel/utility/normal_dialog.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -12,6 +17,7 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
   // Field
   File file;
+  String name, email, password, uid, url;
 
   // Method
 
@@ -20,6 +26,9 @@ class _RegisterState extends State<Register> {
     return Container(
       margin: EdgeInsets.only(left: 30.0, right: 30.0),
       child: TextField(
+        onChanged: (String string) {
+          name = string.trim();
+        },
         style: TextStyle(color: color),
         decoration: InputDecoration(
           enabledBorder:
@@ -43,6 +52,10 @@ class _RegisterState extends State<Register> {
     return Container(
       margin: EdgeInsets.only(left: 30.0, right: 30.0),
       child: TextField(
+        keyboardType: TextInputType.emailAddress,
+        onChanged: (String string) {
+          email = string.trim();
+        },
         style: TextStyle(color: color),
         decoration: InputDecoration(
           enabledBorder:
@@ -66,6 +79,9 @@ class _RegisterState extends State<Register> {
     return Container(
       margin: EdgeInsets.only(left: 30.0, right: 30.0),
       child: TextField(
+        onChanged: (String string) {
+          password = string.trim();
+        },
         style: TextStyle(color: color),
         decoration: InputDecoration(
           enabledBorder:
@@ -98,8 +114,8 @@ class _RegisterState extends State<Register> {
     try {
       var object = await ImagePicker.pickImage(
         source: imageSource,
-        maxWidth: 800.0,
-        maxHeight: 800.0,
+        maxWidth: 200.0,
+        maxHeight: 200.0,
       );
 
       setState(() {
@@ -139,9 +155,78 @@ class _RegisterState extends State<Register> {
   Widget registerButton() {
     return IconButton(
       icon: Icon(Icons.cloud_upload),
-      onPressed: () {},
+      onPressed: () {
+        if (file == null) {
+          normalDialog(context, 'No Image',
+              'Please Click Camera or Gallery for Choose Image');
+        } else if (name == null ||
+            name.isEmpty ||
+            email == null ||
+            email.isEmpty ||
+            password == null ||
+            password.isEmpty) {
+          normalDialog(context, 'Have Space', 'Please Fill Every Blank');
+        } else {
+          registerFirebase();
+        }
+      },
     );
   }
+
+  Future<void> registerFirebase() async {
+    FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+    await firebaseAuth
+        .createUserWithEmailAndPassword(email: email, password: password)
+        .then((response) {
+      print('Register Success');
+      findUID();
+    }).catchError((response) {
+      print('Register False');
+      String title = response.code;
+      String message = response.message;
+      normalDialog(context, title, message);
+    });
+  }
+
+  Future<void> findUID() async {
+    FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+    FirebaseUser firebaseUser = await firebaseAuth.currentUser();
+    uid = firebaseUser.uid;
+    print('uid = $uid');
+    uploadPicture();
+  }
+
+  Future<void> uploadPicture()async{
+
+    Random random = Random();
+    int i = random.nextInt(10000);
+    String namePic = 'avata$i.jpg';
+
+    FirebaseStorage firebaseStorage = FirebaseStorage.instance;
+    StorageReference storageReference = firebaseStorage.ref().child('Avatar/$namePic');
+    StorageUploadTask storageUploadTask = storageReference.putFile(file);
+    
+    url = await (await storageUploadTask.onComplete).ref.getDownloadURL();
+    print('url = $url');
+    insertValueToFirestore();
+
+  }
+
+
+  Future<void> insertValueToFirestore()async{
+
+    Map<String, dynamic> map = Map();
+    map['Name'] = name;
+    map['Uid'] = uid;
+    map['Url'] = url;
+
+    Firestore firestore = Firestore.instance;
+    CollectionReference collectionReference = firestore.collection('Travel');
+    await collectionReference.document().setData(map).then((response){Navigator.of(context).pop();});
+
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
